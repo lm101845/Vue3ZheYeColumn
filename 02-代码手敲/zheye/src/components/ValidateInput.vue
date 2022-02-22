0746ca2d5898c199afec50dc986e7169dee92659
lm101845 <1018457683@qq.com>
2022-02-22 23:13:43 +0800

    解决项目报错问题

---
+++ 02-代码手敲/zheye/src/components/ValidateInput.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: liming
+ * @Date: 2022-02-22 23:01:38
+ * @LastEditTime: 2022-02-22 23:01:39
+ * @FilePath: \Vue3ZheYeColumn\02-代码手敲\zheye\src\components\ValidateInput.vue
+-->
+<template>
+    <h1></h1>
+</template>
+
+<script lang="ts">
+import { defineComponent,reactive,PropType } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
